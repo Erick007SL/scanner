56c4ee23039ba02fa7f8dbe6071c57660832673d
Erick007SL <112082840+Erick007SL@users.noreply.github.com>
2024-04-19 10:33:05 -0300

    Update App.js - leitor

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [qrData, setQrData] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setQrData(data);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Solicitando permissão para acessar a câmera...</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>Sem acesso à câmera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && (
+        <View style={styles.scanData}>
+          <Text style={styles.scanDataText}>Código QR escaneado:</Text>
+          <Text style={styles.qrData}>{qrData}</Text>
+          <Button title={'Escanear novamente'} onPress={() => setScanned(false)} />
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +46,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'center',
+  },
+  scanData: {
+    position: 'absolute',
+    bottom: 16,
+    left: 0,
+    right: 0,
+    padding: 16,
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  scanDataText: {
+    color: '#fff',
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  qrData: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
